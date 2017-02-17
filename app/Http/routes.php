8af0f1a4ab3dba969618e496c23bc7d9752a1708
panhao <748260455@qq.com>
2017-02-17 13:13:57 +0800

    finish layout and routes

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -14,6 +14,10 @@
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/','StaticPagesController@home');
-Route::get('/help','StaticPagesController@help');
-Route::get('/about','StaticPagesController@about');
+//Route::get('/','StaticPagesController@home');
+//Route::get('/help','StaticPagesController@help');
+//Route::get('/about','StaticPagesController@about');
+Route::get('/help','StaticPagesController@help')->name('help');
+Route::get('/','StaticPagesController@home')->name('home');
+Route::get('/about','StaticPagesController@about')->name('about');
+Route::get('signup','UserController@create')->name('signup');
