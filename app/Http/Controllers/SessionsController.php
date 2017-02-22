86b8c1671279f3fd643640595097adebbe221103
panhao <748260455@qq.com>
2017-02-22 13:53:12 +0800

    finish user edit,update,index,and destroy actions

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -25,13 +25,13 @@ class SessionsController extends Controller
        ]);
 
        $credentials = [
-           'email'    => $request->email,
-           'password' => $request->password,
+           'email'    => $request->input('email'),
+           'password' => $request->input('password'),
        ];
 
        if (Auth::attempt($credentials,$request->has('remember'))) {
            session()->flash('success', '欢迎回来！');
-           return redirect()->route('users.show', [Auth::user()]);
+           return redirect()->intended(route('users.show', [Auth::user()]));
        } else {
            session()->flash('danger', '很抱歉，您的邮箱和密码不匹配');
            return redirect()->back();
@@ -43,5 +43,11 @@ class SessionsController extends Controller
       Auth::logout();
       session()->flash('success','您已經成功推出');
       return redirect('login');
+    }
+    public function __construct()
+    {
+      $this->middleware('guest',[
+        'only'=>['create']
+      ]);
     }
 }
